786515864ae169c761a47f3b2bd696bde8ad0438
minato <lock.extinguish@gmail.com>
2025-05-18 22:31:05 +0900

    本番反映

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
+import outputs from "../amplify_outputs.json";
+import { Amplify } from 'aws-amplify';
+Amplify.configure(outputs);
 
+
+Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
